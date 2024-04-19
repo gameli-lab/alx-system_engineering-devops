37d330dba844abc40f2f3f4e1b26c5a98cc9cd00
Benjamin Torfu <btorfu@proton.me>
2024-04-19 23:29:35 +0000

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #Using Puppet, create a file
 
 file { 'school':
-path    => '/tmp/school'
-owner   => 'www-data'
-group   => 'www-data'
-content =>'I love Puppet'
-mode	=> '0744'
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+content =>'I love Puppet',
+mode	=> '0744',
 }
