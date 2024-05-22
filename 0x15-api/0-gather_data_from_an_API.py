5a259641f67cacdd21bf8e89d3c24acd0eb90ded
Benjamin Torfu <btorfu@proton.me>
2024-05-22 13:48:47 +0000

    gather

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,9 +3,9 @@
 This is the API module
 """
 
-from sys import argv
 import json
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     num_total = len(todos)
     print("Employee {} is done with tasks({}/{})".format(
         user.get("name"), num_done, num_total))
-    [print("\t {}".format(t.get("title"))) for t in todos if t.get(
+    [print("     {}".format(t.get("title"))) for t in todos if t.get(
         "completed") is True]
