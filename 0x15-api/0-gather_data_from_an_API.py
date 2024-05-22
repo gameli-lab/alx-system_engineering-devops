31e202eb24ff181fe615588026b93c4de257b6b2
Benjamin Torfu <btorfu@proton.me>
2024-05-22 15:21:17 +0000

    gather

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     num_total = len(todos)
     print("Employee {} is done with tasks({}/{})".format(
         user.get("name"), num_done, num_total))
-    [print("     {}".format(t.get("title"))) for t in todos if t.get(
+    [print("\t {}".format(t.get("title"))) for t in todos if t.get(
         "completed") is True]
