678f1974928c8c187a146ab6434077c0170a4605
Benjamin Torfu <btorfu@proton.me>
2024-06-05 04:44:09 +0000

    subreddits

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+!#/usr/bin/python3
+'''
+API request module
+'''
+
+import requests
+
+def number_of_subscribers(subreddit):
+    ''' Qeuries for the number of subscribers '''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+'''
+if __name__ == "__main__":
+    number_of_subscribers(subreddits)
+'''
